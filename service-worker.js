8338f0403d555034836c401ee17a9d584d0df008
Gabriel Coelho <2509gmcs@gmail.com>
2017-06-08 10:13:39 -0300

    Create service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,25 +1,67 @@
-var CACHE_VERSION = 'app-v1';
-var CACHE_FILES = [
-        '/index.html'
+var cacheName = 'lex-v1';
+
+// Assets to cache
+var assetsToCache = [
+  '/index.html',
+  '/js/Lex.js',
+  '/js/Lex_mapa.js',
+  '/css/Lex.css'
 ];
 
-self.addEventListener('install', function (event) {
-    event.waitUntil(
-        caches.open(CACHE_VERSION)
-            .then(function (cache) {
-                console.log('Opened cache');
-                return cache.addAll(CACHE_FILES);
-            })
-    );
+self.addEventListener('install', function(event) {
+  // waitUntil() ensures that the Service Worker will not
+  // install until the code inside has successfully occurred
+  event.waitUntil(
+    // Create cache with the name supplied above and
+    // return a promise for it
+    caches.open(cacheName).then(function(cache) {
+        // Important to `return` the promise here to have `skipWaiting()`
+        // fire after the cache has been updated.
+        return cache.addAll(assetsToCache);
+    }).then(function() {
+      // `skipWaiting()` forces the waiting ServiceWorker to become the
+      // active ServiceWorker, triggering the `onactivate` event.
+      // Together with `Clients.claim()` this allows a worker to take effect
+      // immediately in the client(s).
+      consolelog("SW status: [CACHEADO].");
+      return self.skipWaiting();
+    })
+  );
 });
 
-self.addEventListener('activate', function(event){
-    console.log(event);
+// Activate event
+// Be sure to call self.clients.claim()
+self.addEventListener('activate', function(event) {
+	// `claim()` sets this worker as the active worker for all clients that
+	// match the workers scope and triggers an `oncontrollerchange` event for
+	// the clients.
+        consolelog("SW status: [ATIVADO].");
+	return self.clients.claim();
 });
 
-self.addEventListener('fetch', function(event){
-  console.log(event.request.url);
-  // return something for each interception
+self.addEventListener('fetch', function(event) {
+  // Ignore non-get request like when accessing the admin panel
+  if (event.request.method !== 'GET') { return; }
+  // Don't try to handle non-secure assets because fetch will fail
+  if (/http:/.test(event.request.url)) { return; }
+
+  // Here's where we cache all the things!
+  event.respondWith(
+    // Open the cache created when install
+    caches.open(cacheName).then(function(cache) {
+      // Go to the network to ask for that resource
+      return fetch(event.request).then(function(networkResponse) {
+        // Add a copy of the response to the cache (updating the old version)
+        cache.put(event.request, networkResponse.clone());
+        // Respond with it
+        return networkResponse;
+      }).catch(function() {
+        // If there is no internet connection, try to match the request
+        // to some of our cached resources
+        return cache.match(event.request);
+      })
+    })
+  );
 });
 
 
