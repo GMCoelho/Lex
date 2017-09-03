fc8c50dd76122d6f37e4a7c2580634ef351a1f80
Gabriel Coelho <2509gmcs@gmail.com>
2017-09-03 15:42:34 -0300

    Update Lex.js

--- js/Lex.js
+++ js/Lex.js
@@ -41,7 +41,14 @@ $(document).ready(function(){
 				}
 				newMarker = L.marker(e.latlng, {draggable:'true'}, {opacity: 1}).addTo(mymap);
 				setMarker(e);
-				newMarker.on('dragend', setMarker);
+					newMarker.on('draged', function(e){
+						newMarker.bindTooltip(
+						"Latitude: " + e.latlng.lat.toString() +
+						" Longitude: " + e.latlng.lng.toString()
+						).openTooltip();
+						conf = true;
+						abreModalCadastro(e);
+					});
 				}
 
 
