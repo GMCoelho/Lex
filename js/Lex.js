ded7b11fdfa1a8898fb1edde80228df534c6e774
Gabriel Coelho <2509gmcs@gmail.com>
2017-09-05 21:51:39 -0300

    Update Lex.js

--- js/Lex.js
+++ js/Lex.js
@@ -29,7 +29,7 @@ $("#meuModal").on('hide.bs.modal', function () {
 			  "showMethod": "fadeIn",
 			  "hideMethod": "fadeOut"
 			}
-		toastr["warning"]("Você não confirmou a localização do posto. <br/><a id="confToast">Clique aqui</a> para confirmar.", "Atenção:")
+		toastr["warning"]("Você não confirmou a localização do posto. <br/><a id='confToast'>Clique aqui</a> para confirmar.", "Atenção:")
 	}
 });
 $("#confToast").on('click', abreModalCadastro(newMarker.getlatlng()));
